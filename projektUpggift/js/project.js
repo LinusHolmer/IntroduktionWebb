5231b27dc12b14cd7d25a69fb4c952a696b4dbbb
LinusHolmer <linus.holmer@stud.sti.se>
2025-01-19 21:03:22 +0100

    mer

--- projektUpggift/js/project.js
+++ projektUpggift/js/project.js
@@ -6,3 +6,16 @@ function toggleMenu() {
     links.style.display = "block";
   }
 }
+
+function filterArticles() {
+  const filter = document.getElementById("filter").value;
+  const articles = document.querySelectorAll(".article");
+
+  articles.forEach((article) => {
+    if (filter === "all" || article.dataset.category === filter) {
+      article.style.display = "block";
+    } else {
+      article.style.display = "none";
+    }
+  });
+}
